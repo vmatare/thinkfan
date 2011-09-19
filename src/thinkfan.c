d49637ea9de1a4d5cc751e29aea27329c48c57ee
Victor Mataré <matare@lih.rwth-aachen.de>
2011-09-20 00:40:49 +0200

    fix comment

--- src/thinkfan.c
+++ src/thinkfan.c
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with thinkfan.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
  * Although it's generally considered bad style, I do make heavy use of
