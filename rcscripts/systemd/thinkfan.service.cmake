e2107b40f5ac7c1be3c3f223acad2421c29bd329
Victor Mataré <matare@lih.rwth-aachen.de>
2016-09-26 21:07:08 +0200

    setup systemd service file with CMake

---
+++ rcscripts/systemd/thinkfan.service.cmake
@@ -0,0 +1,12 @@
+[Unit]
+Description=Thinkfan, the minimalist fan control program
+
+[Service]
+Type=forking
+ExecStart=@CMAKE_INSTALL_PREFIX@/sbin/thinkfan $THINKFAN_ARGS
+PIDFile=/run/thinkfan.pid
+ExecReload=/bin/kill -HUP $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+Also=thinkfan-wakeup.service
