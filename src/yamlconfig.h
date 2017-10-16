8144f019cded016ce610f6a42cc77041f444e237
Victor Mataré <matare@lih.rwth-aachen.de>
2017-10-16 18:18:09 +0200

    wtf_ptr: fix operator = signature

--- src/yamlconfig.h
+++ src/yamlconfig.h
@@ -20,7 +20,7 @@ using namespace std;
  * std::unique_ptr. I'm sorry.
  */
 template<typename T>
-class wtf_ptr : public shared_ptr<unique_ptr<T>> {
+class wtf_ptr : public shared_ptr<unique_ptr<T>>, public enable_shared_from_this<unique_ptr<T>> {
 public:
 	using shared_ptr<unique_ptr<T>>::shared_ptr;
 
@@ -47,8 +47,8 @@ public:
 	{ return this->get()->operator * (); }
 
 	template<typename T1>
-	wtf_ptr<T> &operator = (wtf_ptr<T1>&& ptr) noexcept
-	{ return this->shared_ptr<T>::operator = (std::move(ptr)); }
+	wtf_ptr<T> &operator = (const wtf_ptr<T1>& ptr) noexcept
+	{ return this->shared_ptr<unique_ptr<T>>::operator = (ptr); }
 };
 
 
